e663767c96a50ef9d471b7586892dcab5ec1457c
chunyu618 <b06902048@ntu.edu.tw>
2020-10-29 18:19:03 +0800

    First commit

---
+++ hw2/test/test.c
@@ -0,0 +1,35 @@
+int func(int a, int b)
+{
+	int main;
+    a = 3;
+	main = a * b;
+}
+
+int main()
+{
+	/* 1: this is just a sample */
+    int c, d;
+	float f,g;
+	char s[64] = "This is a /*This is a comment inside String*/ string.{!?}";
+    char s2[64] = "There is a \n in this string.";
+    //char s3[64] = "\";
+    g = 3.3;
+    c = 1;
+    d = 3;
+	/* 2: this is a 3-line
+	comment
+	*/
+	f = read();
+    func(c,e);
+	write(d);
+	d = 0;  /* 3: this is * the end\n\n ***/
+    d = 123;
+    /***** 4: this is comment *****/
+    /*
+     * one
+     * two
+     * three
+     */
+     /* /* A */ */
+    return 0;
+}
