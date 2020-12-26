f82d3e56bf7707a124d06767aa4572db2f37c30a
chunyu618 <b06902048@ntu.edu.tw>
2020-12-26 18:56:07 +0800

    1226

--- hw5/output.s
+++ hw5/output.s
@@ -1,24 +1,31 @@
 _start_MAIN:
     addi	sp,sp,-16
-    li  a0,133
 	sd	ra,8(sp)
 	sd	s0,0(sp)
 	addi	s0,sp,16
-    call _write_int
+    la  a0,helloworld
+    call _write_str
     li	a5,0
 	mv	a0,a5
 	ld	ra,8(sp)
 	ld	s0,0(sp)
 	addi	sp,sp,16
+
     addi	sp,sp,-16
-    li  a0,233
 	sd	ra,8(sp)
 	sd	s0,0(sp)
 	addi	s0,sp,16
-    call _write_int
+    la  a0,newLine
+    call _write_str
     li	a5,0
 	mv	a0,a5
 	ld	ra,8(sp)
 	ld	s0,0(sp)
 	addi	sp,sp,16
+
+
 	jr	ra
+
+.data 
+helloworld:  .ascii "Hello Worldfvhiwebvhwbrjvkbweihvbwefkjvbefjkbvhirwebvefjwkbvjekrwbvebrwjkv\0"
+newLine: .ascii "1\n2\n"
